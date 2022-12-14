e3a2df9f08309091b2cd3fbe5c120c842e559901
CHAOSdonut <rick2.0@hotmail.nl>
2022-12-14 10:09:08 +0100

    removed todo

--- src/styles/dfn-panel.css.js
+++ src/styles/dfn-panel.css.js
@@ -1,8 +1,4 @@
 /* dfn popup panel that list all local references to a dfn */
-/**
- * TODO: Revert changes due to https://github.com/w3c/respec/pull/2888 when
- * https://github.com/w3c/css-validator/pull/111 is fixed.
- */
 const css = String.raw;
 
 // Prettier ignore only to keep code indented from level 0.
