42c281b57f715c16d8ab52a962e4a7e4a132b926
Alexander Green <alexander.green@logius.nl>
2025-06-02 11:05:15 +0200

    Vaker versienummers weergeven (#88)
    * Only omit version number if WV
    as opposed to only display with DEF
    
    * lint fix
    
    * WV wel huidige versie laten tonen
    
    Co-authored-by: Tim van der Lippe <TimvdLippe@users.noreply.github.com>

--- src/nlgov/title.js
+++ src/nlgov/title.js
@@ -1,7 +1,7 @@
 export const name = "logius/title";
 
 export async function run(conf) {
-  if (conf.specStatus.toUpperCase() == "DEF" && conf.publishVersion) {
+  if (conf.specStatus.toUpperCase() != "WV" && conf.publishVersion) {
     document.title = `${document.title} ${conf.publishVersion}`;
     conf.title = `${conf.title} ${conf.publishVersion}`;
   }
